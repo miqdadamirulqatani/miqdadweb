dd27ccd1b9e216065ee06bc8189bd978a9fad81f
miqdadamirulqatani <miqdadamirul.qatani@gmail.com>
2024-06-21 10:00:31 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+const nama = "miqdad amirul qatani";
+let umur = 15;
+
+function generateBiodata(){
+    if (umur == 15) {
+    //  INI ADALAH KONDISI PERTAMA
+    console.log(`anda remaja`)
+    }else{
+    // INI ADALAH KONDISI JIKA TIDAK DIPENUHI
+    console.log(`ANDA MASSIH KECIL`)
+    }
+    console.log(`nama saya adalah ${nama} umur saya adalah ${umur}`)
+}
+
+console.log(nama);
+console.log(umur);
+
+generateBiodata();
+
+
+
+
+
+
+
+
+
